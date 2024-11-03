9786de9596acc96b146110b3e6e07c46236ff450
@jfleetwood <john.fleetwood@zenith.com>
2024-11-02 21:27:06 -0500

    Removing test files

--- drizzle/schema/index.ts
+++ drizzle/schema/index.ts
@@ -2,4 +2,4 @@ export { text, uuid, timestamp } from "drizzle-orm/pg-core/columns";
 export { v4 as uniqueID } from 'uuid'; 
 import { pgSchema } from "drizzle-orm/pg-core";
 
-export const dbSchema = pgSchema(process.env.DATABASE_SCHEMA || "public");
+export const dbSchema = pgSchema(process.env.DATABASE_SCHEMA || "fleetwood");
